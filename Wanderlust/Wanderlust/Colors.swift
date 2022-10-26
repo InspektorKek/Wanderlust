2b50495a2a4f683552af8d422480ebf4d1f1490c
Beatriz Gomes <bdecastromaiagomes22@fed.idserve.net>
2022-10-26 15:04:10 +0200

    Do Color Palette

--- Wanderlust/Wanderlust/Colors.swift
+++ Wanderlust/Wanderlust/Colors.swift
@@ -9,5 +9,22 @@ import SwiftUI
 
 extension Color {
     static let salad = Color("salad")
+    static let WinColor = Color("WinColor")
+    static let LightBlue = Color("LightBlue")
+    static let GrayColor = Color("GrayColor")
+    static let BlueCard = Color("BlueCard")
+    static let ClearBlue = Color("ClearBlue")
+    static let DarkGray = Color("DarkGray")
+    static let DarkGreenCard = Color("DarkGreenCard")
+    static let GrayButton = Color("GrayButton")
+    static let LightGreenCard = Color("LightGreenCard")
+    static let OrangeCard = Color("OrangeCard")
+    static let PurpleCard = Color("PurpleCard")
+    static let Red = Color("Red")
+    static let YellowCard = Color("YellowCard")
+    static let WrongRed = Color("WrongRed")
+    static let AccentColor = Color("AccentColor")
+
+
 }
 
