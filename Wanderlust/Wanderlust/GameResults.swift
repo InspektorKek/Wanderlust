febde4306d5cff9829b051fe2865c611e6945ee3
Beatriz Gomes <bdecastromaiagomes22@fed.idserve.net>
2022-10-27 15:19:17 +0200

    Edit results screen

--- Wanderlust/Wanderlust/GameResults.swift
+++ Wanderlust/Wanderlust/GameResults.swift
@@ -16,24 +16,24 @@ struct GameResults: View {
         NavigationStack {
             VStack(content: {
                 
+                Spacer()
+                
                 Text("Game Over")
                     .font(.system(size: 40, weight: .bold, design: .monospaced))
-                    .offset(y: 30)
                 
                 
                 Image("looser")
                     .resizable()
                     .frame(width: 250, height: 400)
-                    .offset(y: 10)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color.white)
                 
                 
                 Text("You can do better!")
                     .font(.system(size: 25, design: .monospaced))
                     .foregroundColor(.black)
-                    .offset(y: -20)
                 
+                Spacer()
                 
                 Button(action: {
                     print("sone")
@@ -49,7 +49,6 @@ struct GameResults: View {
                 .foregroundColor(.white)
                 .font(.system(size: 25, weight: .bold))
                 .tint(.yellow)
-                .offset(y: -15)
                 
                 Button(action: {
                     presentationMode.wrappedValue.dismiss()
