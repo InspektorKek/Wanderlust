01ccdc1fcece350b3055886001995c05585daa37
martinacasillo <113692417+martinacasillo@users.noreply.github.com>
2022-10-28 18:50:41 +0200

    Updates of the story

--- Wanderlust/Wanderlust/Story/Playthegame.swift
+++ Wanderlust/Wanderlust/Story/Playthegame.swift
@@ -10,45 +10,20 @@ import SwiftUI
 struct Playthegame: View {
     var body: some View {
         VStack {
-                HStack {
-                    VStack {
-                        Image ("chili")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 80)
-                        
-                        Image ("Taco")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 130, height: 160)
-                    }
-                    
-                    Image ("Man")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                    
-                }
-                .padding(.all)
-            
             HStack {
-                Image ("ManTwo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                
-                    
-                
                 VStack {
-                    Image ("skull")
+                    Image ("SkullFlowerss")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width:200 , height: 140)
                     
-                    Image ("chili")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:90 , height: 80)
+                    
                     
                 }
+                
+                
+                
+                
+                
             }
             Button {
                 print("Button pressed")
@@ -69,11 +44,12 @@ struct Playthegame: View {
             .cornerRadius(10)
         }
     }
+}
     
-}
-
-struct Playthegame_Previews: PreviewProvider {
-    static var previews: some View {
-        Playthegame()
+    
+    struct Playthegame_Previews: PreviewProvider {
+        static var previews: some View {
+            Playthegame()
+        }
     }
-}
+
